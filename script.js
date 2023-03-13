a735c02ed61004e498338703cd5f5380e6044c7b
Eneias Júnior <jrprett@gmail.com>
2023-03-13 12:19:24 -0300

    Changed function order

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function add(num1, num2) {
+function operate(operator, num1, num2){
+    if(operator == '+') add(num1, num2)
+    else if(operator == "-") subtract(num1, num2)
+    else if(operator == "/") divide(num1, num2)
+    else multiply(num1, num2)
+}
+
+function add(num1, num2){
     return num1 + num2
 }
 
-function subtract(num1, num2) {
+function subtract(num1, num2){
     return num1 - num2
 }
 
@@ -12,15 +19,4 @@ function divide(num1, num2){
 
 function multiply(num1, num2){
     return num1 * num2
-}
-
-function operate(operator, num1, num2){
-    if(operator == '+'){
-        add(num1, num2)
-    } else if(operator == "-"){
-        subtract(num1, num2)
-    } else if(operator == "/"){
-        divide(num1, num2)
-    } else {multiply(num1, num2)}
-    return `${num1} ${operator} ${num2}`
 }
