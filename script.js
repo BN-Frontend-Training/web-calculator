340bd0fc768e873912b012f674227a9b013189b2
Eneias Júnior <jrprett@gmail.com>
2023-03-14 08:44:57 -0300

    Display function tests

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ function divide(num1, num2){
 function multiply(num1, num2){
     return num1 * num2
 }
+
+function calculatorDisplay(){
+    /*
+    Take the click input
+    Put it on display: change the html display container!
+    
+    */
+
+}
+
+const btn = document.querySelectorAll('button');
+const displayChange = document.getElementById('display');
+operators = ["X", "-", "+", "/", "="]
+
+btn.forEach(function(i){
+  i.addEventListener("click", function(e){
+    if(!operators.includes(e.target.innerHTML))
+    displayChange.value = e.target.innerHTML;
+  })
+})
